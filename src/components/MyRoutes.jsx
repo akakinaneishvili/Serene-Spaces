2c609c8551607584e9a87a0991d6f4a2a4d165c0
akakinaneishvili <naneishviliakaki@gmail.com>
2026-05-28 09:07:32 +0400

    siaxl

--- src/components/MyRoutes.jsx
+++ src/components/MyRoutes.jsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Leyout from "../leyout/Leyout";
-import Home from "../pages/Home";
+import Home from "../Pages/Home";
 import Collections from "../Pages/Collections";
-import Shopping from "../pages/Shopping";
-import About from "../pages/About";
-import ProductDetails from "../pages/ProductDetails";
-import Registration from "../components/Registration";
-import LogIn from "./LogIn";
+import Shopping from "../Pages/Shopping";
+import About from "../Pages/About";
+import ProductDetails from "../Pages/ProductDetails";
+import Registration from "./Registration"; // <-- პირდაპირ იქვე დევს!
+import LogIn from "./LogIn"; // <-- პირდაპირ იქვე დევს!
+
 function MyRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Leyout />}>
         <Route index element={<Home />} />
-
         <Route path="product/:id" element={<ProductDetails />} />
-
         <Route path="collections" element={<Collections />} />
         <Route path="Shopping" element={<Shopping />} />
         <Route path="About" element={<About />} />
